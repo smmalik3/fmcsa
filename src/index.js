4e8db506f072aefe375e518bf94e384a1dd103ce
Salman Malik <salmalik@deloitte.com>
2021-05-18 11:52:04 -0400

    verbiage updates

--- src/index.js
+++ src/index.js
@@ -52,20 +52,23 @@ function goToPivLogin() {
     window.location="https://federatetest.smext.faa.gov/affwebservices/public/saml2sso?SPID=http://10.75.161.205:8080/&RelayState=http://10.75.161.205:8080/group/guest/home"
 }
 
-//Toggle Authenticated View
+//Toggle Authenticated View - Not actually being loaded from here, used in script tag on landing.html
 function isNotLoggedIn() {
     console.log("this method ran on page load");
     var x = document.getElementById("app-list");
     var y = document.getElementById("login-items");
     var z = document.getElementById("app-list-message");
+    var a = document.getElementById("user-welcome");
     if(!document.URL.match('#authenticated')){
         x.style.display = "none";
         y.style.display = "block";
         z.style.display = "block"
+        a.style.display = "none"
     } else {
         x.style.display = "block";
         y.style.display = "none";
         z.style.display = "none"
+        a.style.display = "block"
     }
 }
 
